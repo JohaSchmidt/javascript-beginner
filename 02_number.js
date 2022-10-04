57f856ed440a319af5376e2f3f4960a6acdddc6e
angren@gmx.net <angren@gmx.net>
2022-10-04 23:32:42 +0200

    1

---
+++ 02_number.js
@@ -0,0 +1,61 @@
+//Number
+// const num = 42 // integer
+// const float = 42.42 //
+// const pow = 10e2 //
+// console.log(pow)
+//
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Math.pow(2,53)-1);
+// console.log(Number.MIN_SAFE_INTEGER)
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.POSITIVE_INFINITY)
+// console.log(Number.NEGATIVE_INFINITY)
+// console.log(2/0)
+// console.log(Number.NaN) //Not a Number
+// console.log(typeof NaN)
+// const weird = 2/undefined
+// console.log(isNaN(weird))
+// console.log(isNaN(42))
+// console.log(Number.isFinite(Infinity))
+//
+// const stringInt = '40'
+// const stringFloat = '42.42'
+// console.log(Number.parseInt(stringInt) +2);
+// console.log(Number(stringInt) +2);
+// console.log(+stringInt + 2)
+//
+// console.log(parseFloat(stringFloat)+2)
+// console.log(+stringFloat +2)
+
+// console.log(0.2+0.4)
+// console.log(2/5 + 1/5)
+// console.log(parseFloat((0.4+0.2).toFixed(1)))
+
+// 2 BigInt
+
+//console.log(900719925474099189n)
+
+// console.log(10n - 4) // ERROR
+// console.log(parseInt(10n) -4)
+// console.log(10n - BigInt(4))
+
+// 3 MATH
+
+
+// console.log(Math.PI)
+// console.log(Math.E)
+// console.log(Math.pow(5, 3))
+// console.log(Math.abs(-42))
+// console.log(Math.max(42, 12, 422))
+// console.log(Math.floor(4.9))
+// console.log(Math.ceil(4.9))
+// console.log(Math.round(4.9))
+// console.log(Math.trunc(4.9))
+
+// 4 Example
+// function getRandomBetween(min, max){
+//     return Math.floor(Math.random() * (max-min +1) + min)
+// }
+//
+// console.log(getRandomBetween(10, 42));
